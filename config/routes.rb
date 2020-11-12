9265f3cf33ecb885eca4fbd550963bf2516b3087
Alex Price <alexprice009@gmail.com>
2020-11-12 14:46:38 +0000

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
+  resources :cocktails, except: [ :index, :update, :destroy]
+  resources :doses, only: [ :create, :new, :destroy]
 end
